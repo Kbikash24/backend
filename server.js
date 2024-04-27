dd2ad7c11810e604b55cccf9f9f474777d171c65
Kbikash24 <bikashk11@gmail.com>
2024-04-27 18:13:46 +0530

    task

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    // Parse the request URL
+    const url = req.url;
+
     // Set the response header with Content-Type
     res.writeHead(200, {'Content-Type': 'text/plain'});
     
-    // Log my name to the console
-    console.log("My name is Bikash");
-
-    // Write my name as the response
-    res.end('My name is Bikash\n');
+    // Handle different URLs
+    if (url === '/home') {
+        res.end('Welcome home\n');
+    } else if (url === '/about') {
+        res.end('Welcome to About Us page\n');
+    } else if (url === '/node') {
+        res.end('Welcome to my Node.js project\n');
+    } else {
+        res.end('404 Not Found\n'); // Return 404 for other URLs
+    }
 });
 
 server.listen(4000, () => {
