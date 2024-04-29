545fd3202aa2ced3561f7f9c82328017a6a76060
Kbikash24 <bikashk11@gmail.com>
2024-04-29 20:56:54 +0530

    task

--- server.js
+++ server.js
@@ -1,21 +1,61 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    // Parse the request URL
     const url = req.url;
+    const method = req.method;
 
-    // Set the response header with Content-Type
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Handle different URLs
-    if (url === '/home') {
-        res.end('Welcome home\n');
-    } else if (url === '/about') {
-        res.end('Welcome to About Us page\n');
-    } else if (url === '/node') {
-        res.end('Welcome to my Node.js project\n');
+    if (url === '/') {
+        fs.readFile('message.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+            
+            let messages = data ? data.split('\n') : [];
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title></head>');
+            res.write('<body>');
+            res.write('<h1>Messages</h1>');
+            res.write('<ul>');
+            messages.forEach((message) => {
+                if (message.trim() !== '') {
+                    res.write('<li>' + message + '</li>');
+                }
+            });
+            res.write('</ul>');
+            res.write('<form action="/message" method="POST">');
+            res.write('<input type="text" name="message">');
+            res.write('<button type="submit">Send</button>');
+            res.write('</form>');
+            res.write('</body></html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.appendFile('message.txt', message + '\n', (err) => {
+                if (err) {
+                    console.error(err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                    return;
+                }
+                res.writeHead(302, { 'Location': '/' });
+                return res.end();
+            });
+        });
     } else {
-        res.end('404 Not Found\n'); // Return 404 for other URLs
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page Not Found');
     }
 });
 
